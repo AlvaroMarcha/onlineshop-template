dcf6f2f234f9bb7d513b0f164196ca129d0afff4
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-19 21:53:00 +0200

    Updates to code header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Menubar, ImageModule],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Tienda',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Cat 1',
+            icon: 'pi pi-bolt',
+          },
+          {
+            label: 'Cat 2',
+            icon: 'pi pi-server',
+          },
+          {
+            label: 'Cat 3',
+            icon: 'pi pi-pencil',
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      },
+      {
+        label: 'Nosotros',
+        icon: 'pi pi-info-circle',
+      },
+    ];
+  }
 }
