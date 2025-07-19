8c1f7dec1d928c04d21ec9d97c5eff4d2067ea12
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-19 20:03:46 +0200

    Create init view

---
+++ src/app/views/init-page/init-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-init-page',
+  imports: [],
+  templateUrl: './init-page.html',
+  styleUrl: './init-page.css'
+})
+export class InitPage {
+
+}
