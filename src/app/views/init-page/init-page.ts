1c12b5ca5defad09047cac000440e9b069a04588
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-27 14:19:03 +0200

    Create init page design

--- src/app/views/init-page/init-page.ts
+++ src/app/views/init-page/init-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { AvatarModule } from 'primeng/avatar';
+import { DividerModule } from 'primeng/divider';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-init-page',
-  imports: [],
+  imports: [AnimateOnScrollModule, AvatarModule, DividerModule, CardModule],
   templateUrl: './init-page.html',
-  styleUrl: './init-page.css'
+  styleUrl: './init-page.css',
+  standalone: true,
 })
-export class InitPage {
-
-}
+export class InitPage {}
