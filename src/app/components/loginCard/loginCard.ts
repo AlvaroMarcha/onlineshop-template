b25fabc018717a7dfb5a18f28363c65140632812
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-29 18:45:00 +0200

    Add button to register form in login form

--- src/app/components/loginCard/loginCard.ts
+++ src/app/components/loginCard/loginCard.ts
@@ -4,6 +4,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginCard',
@@ -20,4 +21,6 @@ import { ButtonModule } from 'primeng/button';
 export class LoginCard {
   userValue!: string;
   passwordValue!: string;
+
+  constructor(public router: Router) {}
 }
