c96ab551d0c73194b1b93ce443e5e9788319ba4c
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-30 22:10:06 +0200

    Add better components

--- src/app/views/init-page/init-page.ts
+++ src/app/views/init-page/init-page.ts
@@ -3,10 +3,17 @@ import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { AvatarModule } from 'primeng/avatar';
 import { DividerModule } from 'primeng/divider';
 import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button'; // Import ButtonModule for p-button
 
 @Component({
   selector: 'app-init-page',
-  imports: [AnimateOnScrollModule, AvatarModule, DividerModule, CardModule],
+  imports: [
+    AnimateOnScrollModule,
+    AvatarModule,
+    DividerModule,
+    CardModule,
+    ButtonModule,
+  ],
   templateUrl: './init-page.html',
   styleUrl: './init-page.css',
   standalone: true,
