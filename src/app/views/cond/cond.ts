f7eac96e2beb7ec132d8cf4eb11e633e05f2550b
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-30 20:53:51 +0200

    Create Terms of use view

---
+++ src/app/views/cond/cond.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cond',
+  imports: [],
+  templateUrl: './cond.html',
+  styleUrl: './cond.css',
+})
+export class Cond {}
