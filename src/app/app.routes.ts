238313b837634cfcf061ea353fa0c1f3e94a6599
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-30 20:59:08 +0200

    Add legal routes to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { InitPage } from './views/init-page/init-page';
 import { Login } from './views/login/login';
 import { Register } from './views/register/register';
+import { Terms } from './views/terms/terms';
+import { Priv } from './views/priv/priv';
+import { Notice } from './views/notice/notice';
+import { Cond } from './views/cond/cond';
 
 export const routes: Routes = [
   { path: '', component: InitPage },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'terms', component: Terms },
+  { path: 'priv', component: Priv },
+  { path: 'notice', component: Notice },
+  { path: 'cond', component: Cond },
 ];
