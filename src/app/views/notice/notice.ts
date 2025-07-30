323f16121b7a2e160bdb1547ece07dafc6408a9c
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-07-30 20:54:29 +0200

    Create Legal notice view

---
+++ src/app/views/notice/notice.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notice',
+  imports: [],
+  templateUrl: './notice.html',
+  styleUrl: './notice.css',
+})
+export class Notice {}
