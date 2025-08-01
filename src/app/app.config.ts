4ea461890de0f50097d3002eafdc739cc4e8daaf
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-08-01 17:35:26 +0200

    Config translate module and add translates file (es, en)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,40 +1,45 @@
 import {
   ApplicationConfig,
   importProvidersFrom,
-  provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
+export function httpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
+      ripple: true,
       theme: {
         preset: Aura,
       },
     }),
-    provideHttpClient(),
+
     importProvidersFrom(
       TranslateModule.forRoot({
+        defaultLanguage: 'es',
         loader: {
           provide: TranslateLoader,
-          useFactory: () =>
-            new TranslateHttpLoader({
-              prefix: '/i18n/',
-              suffix: '.json',
-            }),
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
         },
       })
     ),
