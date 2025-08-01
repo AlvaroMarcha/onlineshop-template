de3ff3e16eb02ecd5546d362238c58a48fcc805d
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-08-01 12:39:36 +0200

    Create PrimengModule to export components

---
+++ src/app/shared/primeng/primeng-module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Menubar } from 'primeng/menubar';
+import { ImageModule } from 'primeng/image';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
+import { InputNumber } from 'primeng/inputnumber';
+import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CheckboxModule } from 'primeng/checkbox';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Menubar,
+    Menubar,
+    ImageModule,
+    TieredMenuModule,
+    ButtonModule,
+    DialogModule,
+    ToastModule,
+    InputNumber,
+    DataViewModule,
+    TagModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    InputMaskModule,
+    CheckboxModule,
+  ],
+  exports: [
+    Menubar,
+    ImageModule,
+    TieredMenuModule,
+    ButtonModule,
+    DialogModule,
+    ToastModule,
+    InputNumber,
+    DataViewModule,
+    TagModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    InputMaskModule,
+    CheckboxModule,
+  ],
+})
+export class PrimengModule {}
