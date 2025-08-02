82f7a3bc7adcf5696875e4ebc33f19b85cf658c9
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-08-02 09:51:03 +0200

    Add home page translates

--- src/app/views/init-page/init-page.ts
+++ src/app/views/init-page/init-page.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { AnimateOnScrollModule } from 'primeng/animateonscroll';
-import { AvatarModule } from 'primeng/avatar';
-import { DividerModule } from 'primeng/divider';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button'; // Import ButtonModule for p-button
+import { Component, OnInit } from '@angular/core';
+import { PrimengModule } from '../../shared/primeng/primeng-module';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language-service';
 
 @Component({
   selector: 'app-init-page',
-  imports: [
-    AnimateOnScrollModule,
-    AvatarModule,
-    DividerModule,
-    CardModule,
-    ButtonModule,
-  ],
+  imports: [PrimengModule, TranslateModule],
   templateUrl: './init-page.html',
   styleUrl: './init-page.css',
   standalone: true,
 })
-export class InitPage {}
+export class InitPage implements OnInit {
+  t!: Record<string, string>;
+  constructor(private lang: LanguageService) {}
+
+  async ngOnInit() {
+    this.t = await this.lang.tMany([
+      'home.banner.button1',
+      'home.banner.button2',
+    ]);
+  }
+}
