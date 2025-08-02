0515e1e1469b537ae9f2784930b6fdd9886ea0ac
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-08-02 09:52:29 +0200

    Add footer page translates

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PrimengModule } from '../../shared/primeng/primeng-module';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, PrimengModule],
+  imports: [RouterLink, PrimengModule, TranslateModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
