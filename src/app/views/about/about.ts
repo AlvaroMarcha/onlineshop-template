0662590be5f598ef1aff9a1a6839adf67614b6db
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-08-28 10:45:15 +0200

    Add Carousel component in about view

--- src/app/views/about/about.ts
+++ src/app/views/about/about.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { PrimengModule } from '../../shared/primeng/primeng-module';
+import { CarouselComponent } from '../../components/gallery/carousel/carousel';
 
 @Component({
   selector: 'app-about',
-  imports: [TranslateModule, CommonModule, PrimengModule],
+  imports: [TranslateModule, CommonModule, PrimengModule, CarouselComponent],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
