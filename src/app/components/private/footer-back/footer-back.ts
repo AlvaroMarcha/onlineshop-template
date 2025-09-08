586c877603987135ed44e43cf68498ab278a786a
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-08 22:19:32 +0200

    Create basic footer back component

---
+++ src/app/components/private/footer-back/footer-back.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { PrimengModule } from '../../../shared/primeng/primeng-module';
+
+@Component({
+  selector: 'app-footer-back',
+  imports: [PrimengModule],
+  templateUrl: './footer-back.html',
+  styleUrl: './footer-back.css',
+})
+export class FooterBack {}
