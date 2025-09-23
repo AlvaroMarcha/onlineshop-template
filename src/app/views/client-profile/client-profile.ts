39af16a6cdb35e9829ca108736e074e7b1d246e5
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-23 21:41:28 +0200

    Create client-profile view

---
+++ src/app/views/client-profile/client-profile.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-profile',
+  imports: [],
+  templateUrl: './client-profile.html',
+})
+export class ClientProfile {}
