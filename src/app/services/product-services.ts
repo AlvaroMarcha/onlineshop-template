e09d1d1ed51ef3041af5b953d0b63f72f168a8d1
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-28 20:33:54 +0200

    Add ProductServices for product API interactions

---
+++ src/app/services/product-services.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../type/types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductServices {
+  private apiProducts = environment.urls.productsUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiProducts}/products`);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiProducts}/products/${id}`);
+  }
+}
