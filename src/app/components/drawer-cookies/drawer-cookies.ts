25179681503176a8089c5ce708c0e40e8699ab0d
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-28 14:12:35 +0200

    Refactor DrawerCookies component: initialize preferences in constructor and enhance cookie management

--- src/app/components/drawer-cookies/drawer-cookies.ts
+++ src/app/components/drawer-cookies/drawer-cookies.ts
@@ -3,13 +3,24 @@ import { PrimengModule } from '../../shared/primeng/primeng-module';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
+const COOKIE_VERSION = '1.0';
 @Component({
   selector: 'app-drawer-cookies',
   imports: [PrimengModule, FormsModule],
   templateUrl: './drawer-cookies.html',
 })
 export class DrawerCookies {
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    const saved = localStorage.getItem('cookie-preferences');
+    const savedVersion = localStorage.getItem('cookie-version');
+
+    if (saved && savedVersion === COOKIE_VERSION) {
+      this.preferences = JSON.parse(saved);
+      this.visible = false;
+    } else {
+      this.visible = true;
+    }
+  }
 
   visible: boolean = true;
 
@@ -44,6 +55,28 @@ export class DrawerCookies {
   savePreferences() {
     console.log('Preferencias guardadas:', this.preferences);
     this.visible = false;
-    // Aquí podrías guardar en localStorage o backend
+
+    //Save cookies
+    localStorage.setItem(
+      'cookie-preferences',
+      JSON.stringify(this.preferences)
+    );
+
+    localStorage.setItem('cookie-version', COOKIE_VERSION);
+
+    //Save during 1 year
+    document.cookie = `cookiePrefs=${encodeURIComponent(
+      JSON.stringify(this.preferences)
+    )}; path=/; max-age=31536000`;
+
+    // TODO REVISAR!!
+    // Upload script for google analytics
+    if (this.preferences.stats) {
+      // this.loadAnalytics();
+    }
+    //Upload script for marketing
+    if (this.preferences.marketing) {
+      // this.loadMarketing();
+    }
   }
 }
